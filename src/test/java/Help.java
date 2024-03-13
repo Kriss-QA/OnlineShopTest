097bc11f52c2a719424a2a01c6a58daa9f5060b3
Kriss-QA <kris.pho@yandex.ru>
2024-03-13 12:40:17 +0300

    Initial commit

---
+++ src/test/java/Help.java
@@ -0,0 +1,19 @@
+public class Help {
+
+    /* Page.locator() создание пользовательских локаторов
+    используются для выполнения действий над элементами, таких как .click .fill
+    Locator getStarted = page.locator("text=Get Started");
+    assertThat(getStarted).hasAttribute("href", "/docs/intro");
+    getStarted.click();
+
+    У Playwright есть концепция BrowserContext , которая представляет собой изолированный в памяти
+    профиль браузера.
+    Рекомендуется создавать новый BrowserContext для каждого теста, чтобы они не мешали друг другу.
+
+    Browser browser = playwright.chromium().launch();
+    BrowserContext context = browser.newContext();
+    Page page = context.newPage();
+
+
+     */
+}
---
+++ src/test/java/ShopTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ShopTest extends Base {
+    @Test
+    public void newTest() {
+        assertTrue(true);
+    }
+}
